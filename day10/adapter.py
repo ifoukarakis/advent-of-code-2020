c679886de07326ab6bff6636a2bd90cd4954eb7a
Ioannis Foukarakis <ioannis.foukarakis@gmail.com>
2020-12-10 21:55:44 +0200

    Day 10

---
+++ day10/adapter.py
@@ -0,0 +1,21 @@
+from collections import Counter, defaultdict
+FILENAME = 'example.txt'
+FILENAME = 'input.txt'
+
+lines = open(FILENAME, 'r').readlines()
+
+adapters = sorted([int(line.strip()) for line in lines])
+
+device = max(adapters) + 3
+skips = [adapters[i] - adapters[i-1] for i in range(1, len(adapters))]
+counter = Counter(skips)
+
+print(counter)
+print((counter.get(1) + 1) * (counter.get(3) + 1))
+
+values = [0] + adapters
+possibilities = {values[-1]: 1}
+for a in reversed(values[:-1]):
+    possibilities[a] = sum(possibilities.get(x, 0) for x in (a + 1, a + 2, a + 3))
+
+print(possibilities[0])
