df9115fd6a7d7b2f9e4ac0c0fb73aeed52e09701
Ioannis Foukarakis <ioannis.foukarakis@gmail.com>
2020-12-24 00:40:15 +0200

    Day 23

--- day23/solve.py
+++ day23/solve.py
@@ -1,12 +1,18 @@
+import tqdm
+
+
 class Cup:
     def __init__(self, value, next):
         self.value = value
         self.next = next
 
+    def __repr__(self):
+        return str(self.value)
+
 
 class Game:
-    def __init__(self, state):
-        self._values = [int(i) for i in state]
+    def __init__(self, values):
+        self._values = values
         self._index = {}
         self.head = Cup(self._values[0], None)
         tmp = self.head
@@ -34,26 +40,23 @@ class Game:
     def move(self):
         next3 = self._pop3()
         next3_vals = [i.value for i in next3]
-        print(f'pick up: {", ".join([str(i) for i in next3_vals])}')
-        other = set([v for v in self._values if v not in next3_vals and v != self.head.value])
 
         target = self.head.value - 1
-        while target not in other:
+        while target in next3_vals or target <= 0:
             target -= 1
             if target < self.min:
                 target = self.max
 
-        print(f'destination: {target}')
         destination = self._index[target]
         next3[-1].next = destination.next
         destination.next = next3[0]
         self.head = self.head.next
 
-    def result(self):
+    def result(self, short=False):
         result = []
-        tmp = self.head
-        while tmp.value != 1:
-            tmp = tmp.next
+        tmp = self._index[1]
+        if short:
+            return tmp.next.value, tmp.next.next.value
 
         tmp = tmp.next
         while tmp.value != 1:
@@ -75,15 +78,29 @@ class Game:
         return " ".join(cups)
 
 
-# g = Game('389125467')
-g = Game('583976241')
-for i in range(100):
-    print(f'-- move {i+1} --')
-    print(f'Cups: {g}')
+# Example:
+# input, loops = '389125467', 10
+# Input
+input, loops = '583976241', 100
+
+values = [int(i) for i in input]
+g = Game(values)
+for i in range(loops):
+    # print(f'-- move {i+1} --')
+    # print(f'Cups: {g}')
     g.move()
-    print()
+    # print()
 
 print('-- final --')
 print(f'Cups: {g}')
 
-print(f'Result: {g.result()}')
+print(f'Part 1: {g.result()}')
+
+g = Game(values + [i for i in range(max(values)+1, 1000001)])
+for i in tqdm.tqdm(range(10000000)):
+    g.move()
+
+a, b = g.result(short=True)
+
+print(f'Part 2: {a} * {b} = {a*b}')
+
